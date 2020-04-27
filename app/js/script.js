36bc8a963c5d7bf9e455ba746f36bb27aa37581a
shini322 <33940422+shini322@users.noreply.github.com>
2020-04-27 12:30:22 +0300

    123

---
+++ app/js/script.js
@@ -0,0 +1,23 @@
+$("#earnings-phone").mask("+7(999) 999-99-99");
+
+$("#earnings-reservation-phone").mask("+7(999) 999-99-99");
+
+$("#employment-phone").mask("+7(999) 999-99-99");
+
+$("#contacs-phone").mask("+7(999) 999-99-99");
+
+$(document).ready(function() {
+    $('.town-select').niceSelect();
+  });
+   
+  $(document).ready(function() {
+    $('.earnings__select-town').niceSelect();
+  });
+ 
+$(".nav-toggle").click(function() {
+  $(this).toggleClass("open");
+  $(".menu-adaptive").slideToggle();
+  $("body").toggleClass("js-overflow");
+});
+
+
